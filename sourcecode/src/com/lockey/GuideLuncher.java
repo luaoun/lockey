164e65ffc531f6a1c7be296e87f9be3893b21b0c
luaoun <luaoun@gmail..com>
2014-08-29 16:27:26 +0800

    初始化项目

---
+++ sourcecode/src/com/lockey/GuideLuncher.java
@@ -0,0 +1,15 @@
+package com.lockey;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuideLuncher extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
