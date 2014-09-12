0424f7f662a771f591224b2d2d54d6e6cd9645a7
luaoun <luaoun@gmail..com>
2014-09-12 10:51:46 +0800

    同步代码

---
+++ sourcecode/src/com/lockey/activity/LockeyLauncherGuider.java
@@ -0,0 +1,35 @@
+package com.lockey.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.lockey.R;
+
+public class LockeyLauncherGuider extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guider_layout);
+        Button startButton = (Button) findViewById(R.id.guider_setting_id);
+
+        startButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SettingActivity.class);
+
+                startActivity(intent);
+//                Toast.makeText(getApplicationContext(), "乐奇锁已经开启", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+
+}
---
+++ sourcecode/src/com/lockey/activity/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.lockey.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.lockey.R;
+
+public class SettingActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_layout);
+        
+    }
+}
