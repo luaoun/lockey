c523f10b586aa33b60c08c619e8d2b5e8b33497d
luaoun <luaoun@gmail.com>
2014-09-15 18:02:56 +0800

    同步代码

--- sourcecode/src/com/lockey/activity/SettingActivity.java
+++ sourcecode/src/com/lockey/activity/SettingActivity.java
@@ -1,15 +1,16 @@
 package com.lockey.activity;
 
 import android.app.Activity;
-import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.GridView;
-import android.widget.SimpleAdapter;
+import android.view.View;
+import android.widget.*;
 import com.lockey.R;
 
 import java.util.*;
@@ -17,20 +18,39 @@ import java.util.*;
 /**
  *参数设置活动页面
  */
-public class SettingActivity extends Activity{
+public class SettingActivity extends Activity implements AdapterView.OnItemClickListener{
 
     SimpleAdapter adapter;
+
     GridView gridView;
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_layout);
+
+        if(preferences == null)
+            preferences = getSharedPreferences("setting", Context.MODE_PRIVATE);
+
         gridView = (GridView) findViewById(R.id.gridView);
-        String from[] = new String[]{"item_pic","item_name"};
-        int to[] = new int[]{R.id.item_pic,R.id.item_name};
+        String from[] = new String[]{"app_icon","app_name"};
+        int to[] = new int[]{R.id.app_icon,R.id.app_name};
         adapter = new SimpleAdapter(this,getDate(),R.layout.item,from,to);
+        adapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Object data, final String textRepresentation) {
+                if(view instanceof ImageView && data instanceof Drawable){
+                    ImageView iconView = (ImageView)view;
+                    iconView.setImageDrawable((Drawable) data);
+                    return true;
+                }
+                return false;
+            }
+        });
         gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(this);
     }
 
     private List<Map<String,Object>> getDate() {
@@ -39,7 +59,7 @@ public class SettingActivity extends Activity{
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
         // 通过查询，获得所有ResolveInfo对象.
-        List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(mainIntent, PackageManager.GET_ACTIVITIES);
+        List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(mainIntent, PackageManager.SIGNATURE_MATCH);
         // 调用系统排序 ， 根据name排序
         // 该排序很重要，否则只能显示系统应用，而不能列出第三方应用程序
         Collections.sort(resolveInfos, new ResolveInfo.DisplayNameComparator(getPackageManager()));
@@ -51,20 +71,30 @@ public class SettingActivity extends Activity{
                 String appLabel = (String) reInfo.loadLabel(getPackageManager()); // 获得应用程序的Label
                 Drawable icon = reInfo.loadIcon(getPackageManager()); // 获得应用程序图标
                 Map appInfo = new HashMap();
-                appInfo.put("item_name",appLabel);
-                appInfo.put("item_pic",icon.getCurrent());
+                appInfo.put("app_name",appLabel);
+                appInfo.put("app_icon",icon);
+                appInfo.put("pkg_name",pkgName);
+                appInfo.put("act_name",activityName);
                 installedApps.add(appInfo);
             }
         }
-//        List<PackageInfo> installedPackages = getPackageManager().getInstalledPackages(PackageManager.SIGNATURE_MATCH);
-//        for (int i = 0; i < installedPackages.size(); i++) {
-//            PackageInfo installedPackage = installedPackages.get(i);
-//            Map appInfo = new HashMap<String, Object>();
-//            appInfo.put("item_name", installedPackage.);
-//            appInfo.put("item_pic", installedPackage.applicationInfo.loadIcon(getPackageManager()));
-//            installedApps.add(appInfo);
-//        }
 
         return installedApps;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("main","viewName="+view.getClass().getName()+";viewId="+view.getId());
+        Log.i("main","parentView="+parent.getClass().getName());
+        Log.i("main","position="+position);
+        Log.i("main","id="+id);
+        Map itemObject = (Map)parent.getAdapter().getItem(position);
+        Log.i("main","item="+itemObject.getClass().getName());
+        Log.i("main","item_pkgName="+itemObject.get("pkg_name"));
+        Log.i("main","item_actName="+itemObject.get("act_name"));
+        TextView textView = (TextView)view.findViewById(R.id.app_name);
+        textView.setTextColor(android.graphics.Color.RED);
+
+        SharedPreferences.Editor editor = preferences.edit();
     }
 }
